51d674e239927edb1c99843c165a331ccc736031
ash98 <ashley.burnett@nationalarchives.gsi.gov.uk>
2017-12-22 14:35:41 +0000

    Create prototype function to convert a float into a formatted currency string

--- js/dist/currency-converter-build.js
+++ js/dist/currency-converter-build.js
@@ -830,7 +830,7 @@ function change_fieldset_text() {
 }
 
 function build_century_intro_paragraph(intro, century) {
-    return "<div id='currency-century-intro'>"+ "<p>" + intro + "</p>" +
+    return "<div id='currency-century-intro'>" + "<p>" + intro + "</p>" +
         "<p id='currency-century-intro-read-more'><a href='./" + century + "-century.php'>Read more about the " + century + " century. </a></p></div>";
 }
 
@@ -839,10 +839,7 @@ function currency_output() {
     var currency_formula_return_values = currency_formula();
 
     var century_preview = "";
-    var converted_money_string = currency_formula_return_values.money.toLocaleString('en-GB', {
-        style: 'currency',
-        currency: 'GBP'
-    });
+    var converted_money_string = number_to_pounds_string(currency_formula_return_values.money);
 
     if (user_inputs.century != "21st") {
         century_preview = build_century_intro_paragraph(conversion_data.century_intros[user_inputs.century], user_inputs.century);
@@ -879,7 +876,7 @@ $("#currency-form").submit(function (event) {
     event.preventDefault();
 
     if (check_validation()) {
-        if(window.location.hash == '#currency-result'){
+        if (window.location.hash == '#currency-result') {
             window.location.hash = ''; // Reset so that browser will auto-scroll down again.
         }
         window.location.hash = '#currency-result';
@@ -937,7 +934,7 @@ function check_validation() {
         return false;
     }
 
-    if(year <= 1970){
+    if (year <= 1970) {
         if (shillings < 0 || shillings > 19) {
             set_validation_message("Please enter a whole number between 0 and 19 into the shillings field. ");
             return false;
@@ -948,19 +945,19 @@ function check_validation() {
             return false;
         }
 
-        if(pounds == 0 && old_pence == 0 && shillings == 0){
+        if (pounds == 0 && old_pence == 0 && shillings == 0) {
             set_validation_message("Please enter a number above 0 into at least one field.");
             return false;
         }
 
     }
 
-    if(year > 1970) {
+    if (year > 1970) {
         if (new_pence < 0 || new_pence > 99) {
             set_validation_message("Please enter a whole number between 0 and 99 into the pence field.");
             return false;
         }
-        if(pounds == 0 && new_pence == 0){
+        if (pounds == 0 && new_pence == 0) {
             set_validation_message("Please enter a number above 0 into at least one field.");
             return false;
         }
@@ -974,6 +971,7 @@ function check_validation() {
 
 function old_money_to_new_formula(user_inputs) {
     var mathResult;
+
     user_inputs = user_inputs || get_user_inputs();
 
     if (user_inputs.year <= 1970) {
@@ -1018,19 +1016,19 @@ function currency_formula(user_inputs) {
     if (user_inputs.year < 1975) {
         buying_power_money_value = user_inputs.pounds + (user_inputs.shillings / 20) + (user_inputs.old_pence / 240);
 
-        if(user_inputs.pounds > 0) {
+        if (user_inputs.pounds > 0) {
             bp_string = "£" + user_inputs.pounds;
         }
-        if(user_inputs.shillings > 0){
-            if(user_inputs.pounds > 0) {
+        if (user_inputs.shillings > 0) {
+            if (user_inputs.pounds > 0) {
                 bp_string = bp_string + ", ";
             }
 
             bp_string = bp_string + user_inputs.shillings + "s";
         }
-        if(user_inputs.old_pence > 0) {
-            if(user_inputs.pounds > 0 || user_inputs.shillings > 0){
-                bp_string = bp_string +" & ";
+        if (user_inputs.old_pence > 0) {
+            if (user_inputs.pounds > 0 || user_inputs.shillings > 0) {
+                bp_string = bp_string + " & ";
             }
             bp_string = bp_string + user_inputs.old_pence + "d";
         }
@@ -1042,8 +1040,7 @@ function currency_formula(user_inputs) {
     else {
         buying_power_money_value = user_inputs.pounds + user_inputs.new_pence;
 
-        // Since it will be modern currency (after 1975) we can use toLocaleString to show the money value.
-        bp_string = buying_power_money_value.toLocaleString('en-GB', {style: 'currency', currency: 'GBP'});
+        bp_string = number_to_pounds_string(buying_power_money_value);
     }
 
     return {
@@ -1052,11 +1049,61 @@ function currency_formula(user_inputs) {
         wool: Math.floor(buying_power_money_value / get_wool_price(user_inputs.year)),
         wheat: Math.floor(buying_power_money_value / get_wheat_price(user_inputs.year)),
         wage: Math.floor(buying_power_money_value / get_wage_price(user_inputs.year)),
-        money: currency_money_to_modern_value,
+        money: currency_money_to_modern_value.toFixed(2),
         bp_string: bp_string
     };
 
+}
 
+function number_to_pounds_string(number) {
+
+    var split_number = number.toString().split(".");
+    var pounds_string = split_number[0];
+    var pence_string = split_number[1] || "";
+    var split_number_pounds_length = pounds_string.length;
+
+    var comma = ",";
+    var position_for_comma;
+    var sliced_pounds_string;
+    var output_string;
+
+    // If the £ value is more than £999, we need to add a comma. E.g. 1000 needs to become £1,000.
+    if (split_number_pounds_length > 3) {
+        switch (split_number_pounds_length) {
+
+            case 4:
+                // Numbers 1,000 to 9,999 we insert the comma at index 1
+                position_for_comma = 1;
+                break;
+            case 5:
+                // Numbers 10,000 to 99,999 we insert the comma at index 2
+                position_for_comma = 2;
+                break;
+            case 6:
+                // Numbers 100,000 to 999,999 we insert the comma at index 3
+                position_for_comma = 3;
+                break;
+
+            default:
+                break;
+        }
+
+        sliced_pounds_string = [pounds_string.slice(0, position_for_comma), comma, pounds_string.slice(position_for_comma)].join('');
+
+    }
+    else {
+        sliced_pounds_string = pounds_string;
+    }
+
+    if (pence_string.length === 1) {
+        pence_string = pence_string + "0";
+    }
+    else if (pence_string.length === 0) {
+        pence_string = "00";
+    }
+
+    output_string = "£" + sliced_pounds_string + "." + pence_string;
+    return output_string;
 }
 
 function get_inflation_rate(year) {
--- test/tests.js
+++ test/tests.js
@@ -13,3 +13,22 @@ QUnit.test( "1270 year, £1000 input currency_formula test", function( assert ) 
     var currency_converter_output = old_money_to_new_formula(test_inputs);
     assert.ok( currency_converter_output === 729826.4, "Passed! - £1000 returned 729826.4" );
 });
+
+QUnit.test("Locale String generator", function (assert) {
+
+    assert.equal(number_to_pounds_string(100000), "£100,000.00", "Passed for 100000");
+    assert.equal(number_to_pounds_string(999999), "£999,999.00", "Passed for 999999");
+
+    assert.equal(number_to_pounds_string(10000), "£10,000.00", "Passed for 10000");
+    assert.equal(number_to_pounds_string(99999), "£99,999.00", "Passed for 99999");
+
+    assert.equal(number_to_pounds_string(1000), "£1,000.00", "Passed for 1000");
+    assert.equal(number_to_pounds_string(9999), "£9,999.00", "Passed for 9999");
+
+    assert.equal(number_to_pounds_string(1), "£1.00", "Passed for 1");
+    assert.equal(number_to_pounds_string(1.1), "£1.10", "Passed for 1.1");
+    assert.equal(number_to_pounds_string(1.20), "£1.20", "Passed for 1.20");
+
+
+
+});
