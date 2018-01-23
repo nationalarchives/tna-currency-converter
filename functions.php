494300f2622459007055d5ae77ab36f8cf8a034e
AshTNA <ashleyaburnett@gmail.com>
2018-01-23 14:57:51 +0000

    remove long-form include from functions.php

--- functions.php
+++ functions.php
@@ -3,7 +3,6 @@
 include 'inc/functions-globals.php';
 include 'inc/functions-identify-environment.php';
 include 'inc/functions-child.php';
-include 'inc/functions-long-form.php';
 
 // For Breadcrumbs and URLs
 $environment = identifyEnvironmentFromIP( $_SERVER['SERVER_ADDR'], $_SERVER['REMOTE_ADDR'] );
