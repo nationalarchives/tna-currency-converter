0ea1f08a1df593289e527a5b8af4fabdbdcf155d
ash98 <ashley.burnett@nationalarchives.gsi.gov.uk>
2018-02-12 11:57:34 +0000

    Cleanup develop branch

--- inc/body/body.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'google-tag-manager.inc';
-include 'tna-header.inc';
-include 'tna-nav.php';
-?>
--- inc/footer/footer.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'tna-newsletter.inc';
-include 'tna-footer.inc';
-include 'tna-footer-scripts.inc';
-?>
