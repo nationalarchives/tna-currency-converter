1c4923421e205ceca8b452cf89ec1b51777f3835
AshTNA <ashleyaburnett@gmail.com>
2018-03-07 09:13:08 +0000

    Reformat code

--- inc/functions-globals.php
+++ inc/functions-globals.php
@@ -5,29 +5,31 @@
  *
  * @param string $environment - a string of either 'internal', 'development' or 'external'
  */
-function setThemeGlobals($environment = null) {
-    if ($environment === null) {
-        throw new BadFunctionCallException('setThemeGlobals function must be passed at string that represents the environment');
-    }
+function setThemeGlobals( $environment = null ) {
+	if ( $environment === null ) {
+		throw new BadFunctionCallException( 'setThemeGlobals function must be passed at string that represents the environment' );
+	}
 
-    global $pre_path;
-    global $pre_crumbs;
+	global $pre_path;
+	global $pre_crumbs;
 
-    switch ($environment) {
-        case 'internal':
-        case 'development':
-            $pre_path = '';
-            $pre_crumbs = array('Help with your research' => '/help-with-your-research/',
-                                'Reading old documents' => '/help-with-your-research/reading-old-documents/',
-                                'Currency Converter: 1270 – 2017' => '/');
-            break;
-        case 'external':
-            $pre_crumbs = array(
-	            'Help with your research' => '/help-with-your-research/',
-	            'Reading old documents' => '/help-with-your-research/reading-old-documents/',
-                'Currency Converter: 1270 – 2017' => '/currency-converter/'
-            );
-            $pre_path = '/currency-converter';
-            break;
-    }
+	switch ( $environment ) {
+		case 'internal':
+		case 'development':
+			$pre_path   = '';
+			$pre_crumbs = array(
+				'Help with your research'         => '/help-with-your-research/',
+				'Reading old documents'           => '/help-with-your-research/reading-old-documents/',
+				'Currency Converter: 1270 – 2017' => '/'
+			);
+			break;
+		case 'external':
+			$pre_crumbs = array(
+				'Help with your research'         => '/help-with-your-research/',
+				'Reading old documents'           => '/help-with-your-research/reading-old-documents/',
+				'Currency Converter: 1270 – 2017' => '/currency-converter/'
+			);
+			$pre_path   = '/currency-converter';
+			break;
+	}
 }
