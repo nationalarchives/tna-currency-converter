8b4a73add04317475c6ae8999e6b8e4ac3b6ab79
AshTNA <ashleyaburnett@gmail.com>
2019-08-22 16:14:35 +0100

    Remove base-sass.css ver 1.18 from functions-child.php

--- inc/functions-child.php
+++ inc/functions-child.php
@@ -8,10 +8,7 @@ function dequeue_parent_style() {
 
 // Enqueue styles in correct order
 function tna_child_styles() {
-	wp_register_style('tna-parent-styles', get_template_directory_uri() . '/css/base-sass.css.min', array(),
-		EDD_VERSION, 'all');
 	wp_register_style('tna-child-styles', get_stylesheet_directory_uri() . '/css/currency.min.css', array(), '0.1', 'all');
-	wp_enqueue_style('tna-parent-styles');
 	wp_enqueue_style('tna-child-styles');
 }
 
